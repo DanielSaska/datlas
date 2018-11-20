735253614db4482a9ece62da95e4da152acfaac6
Daniel Saska <daniel@saska.io>
2018-11-20 22:18:23 +0000

    moved ingest files to a more approprate module

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 #!/bin/python3
-from .observatory.ingest_recording import ingest_recording
-from .observatory.ingest_experiment import ingest_experiment
-from .observatory.ingest_group import ingest_group
+from .ingest.recording import ingest_recording
+from .ingest.experiment import ingest_experiment
+from .ingest.group import ingest_group
 from pymongo import MongoClient
 import json
 import os
